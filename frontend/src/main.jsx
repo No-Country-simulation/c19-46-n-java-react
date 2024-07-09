8f6d392fedc1619d93ce79a38f4b2e83b1227f3c
CristianCarballo <cristian.carballo1994@gmail.com>
2024-07-08 21:51:27 -0300

    init, install, config y readme

---
+++ frontend/src/main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </React.StrictMode>
+);
