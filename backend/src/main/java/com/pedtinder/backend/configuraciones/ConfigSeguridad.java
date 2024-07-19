86b6811e28d4cac58730f5f57eb37f996cc102fa
Dalma <p.d.f.1915@gmail.com>
2024-07-19 01:27:33 -0300

    Se modifica configuraciones

--- backend/src/main/java/com/pedtinder/backend/configuraciones/ConfigSeguridad.java
+++ backend/src/main/java/com/pedtinder/backend/configuraciones/ConfigSeguridad.java
@@ -2,18 +2,33 @@ package com.pedtinder.backend.configuraciones;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
-public class ConfigSeguridad {
+@EnableWebSecurity
+public class  ConfigSeguridad {
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
 
         return new BCryptPasswordEncoder();
 
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/usuarios/registro", "/api/ciudades").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
     }
 
 }
