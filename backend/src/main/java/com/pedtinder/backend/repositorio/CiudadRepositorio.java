f32bb79fb406016c054686a9b1f4ab9ea7e5333b
Dalma Ponce <106714820+dalmaponce@users.noreply.github.com>
2024-07-26 22:46:36 -0300

    Delete backend directory

--- backend/src/main/java/com/pedtinder/backend/repositorio/CiudadRepositorio.java
+++
@@ -1,8 +0,0 @@
-package com.pedtinder.backend.repositorio;
-
-import com.pedtinder.backend.entidades.Ciudad;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CiudadRepositorio extends JpaRepository<Ciudad, Long> {
-
-}
